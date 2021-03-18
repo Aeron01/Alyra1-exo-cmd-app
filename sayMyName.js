9dec2a1ea20d71f5b38fa97d232a6e5b57b25f4a
Aeron01 <one.soulreaper@gmail.com>
2021-03-18 14:42:36 +0100

    1er exo fini (et un peut pousser)

---
+++ sayMyName.js
@@ -0,0 +1,9 @@
+const firstName = process.argv[2]
+const lastName = process.argv[3]
+const age = process.argv[4]
+const live = process.argv[5]
+if (process.argv.length !== 6) {
+  console.log('Usage: node sayMyName.js name')
+  process.exit(1)
+}
+console.log(`My first name is ${firstName}, my last name is ${lastName}, im ${age} and i'm living in ${live}.`)
