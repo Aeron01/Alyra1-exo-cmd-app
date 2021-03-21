c9793b976f0140480131d819ce3ba4834e0fde22
Aeron01 <one.soulreaper@gmail.com>
2021-03-21 17:46:46 +0100

    up

--- quiz.js
+++ quiz.js
@@ -7,17 +7,22 @@ let note = 0
 const choix = ['language', 'compilateur']
 const choix2 = ['Vrais', 'Faux']
 
-
-const Qst1 = readlineSync.keyInSelect(choix, 'Question 1: Le C++ est un:')
+console.log('Question 1: Le C++ est un:')
+const Qst1 = readlineSync.keyInSelect(choix)
 Qst1 === 0 ? note++ : note = 0
-const Qst2 = readlineSync.keyInSelect(choix2, 'Question 2: TypeScript est une évolution de Javascript:')
+console.log('Question 2: TypeScript est une évolution de Javascript:')
+const Qst2 = readlineSync.keyInSelect(choix2)
 Qst2 === 0 ? note++ : note = 0
-const Qst3 = readlineSync.keyInSelect(choix2, 'Question 3: Lire les cours avant de faire les exercices est inutile:')
+console.log('Question 3: Lire les cours avant de faire les exercices est inutile:')
+const Qst3 = readlineSync.keyInSelect(choix2)
 Qst3 === 1 ? note++ : note = 0
-const Qst4 = readlineSync.keyInSelect(choix2, 'Question 4: react.js a été developpé par Google:')
+console.log('Question 4: react.js a été developpé par Google:')
+const Qst4 = readlineSync.keyInSelect(choix2)
 Qst4 === 1 ? note++ : note = 0
-const Qst5 = readlineSync.keyInSelect(choix2, 'Question 5: Ethereum est une blockchain publique:')
+console.log('Question 5: Ethereum est une blockchain publique:')
+const Qst5 = readlineSync.keyInSelect(choix2)
 Qst5 === 0 ? note++ : note = 0
+
 
 
 console.log(`Votre note est de ${note} bonne réponse sur 5.`)
