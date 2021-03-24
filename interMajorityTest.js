c60c5ab48ab0514f1fde5aa89742d440db742cc6
Aeron01 <one.soulreaper@gmail.com>
2021-03-24 11:55:07 +0100

    final update

--- interMajorityTest.js
+++ interMajorityTest.js
@@ -9,6 +9,17 @@ if (isNaN(process.argv[2])) {
   console.log(chalk.redBright(`Error: ${process.argv[2]} is not à number.`))
   process.exit(1)
 }
+let checkmajo = Number(process.argv[2])
+if (checkmajo === 0) {
+  console.log(chalk.redBright(`Error: ${checkmajo} is not a proper argument.`))
+  process.exit(1)
+}
+
+if (checkmajo < 0) {
+  console.log(chalk.redBright(`Error: ${checkmajo} is a negative number.`))
+  process.exit(1)
+}
+
 while (true) {
   const check = (age, majo) => {
 
@@ -20,12 +31,27 @@ while (true) {
 
   }
 
-
-  let majo = Number(process.argv[2])
+  let majo = checkmajo
 
   const firstName = readlineSync.question('Quelle est votre Prénom ? ');
+  if (firstName.length === 0) {
+    console.log('Désoler, mais vous devez renseigner obligatoirement votre prénom.')
+    continue
+  }
+
   const lastName = readlineSync.question('Quelle est votre Nom ? ');
+  if (lastName.length === 0) {
+    console.log('Désoler, mais vous devez renseigner obligatoirement votre nom.')
+    continue
+  }
+
   const age = readlineSync.question('Quelle âge avez vous ? ');
+  let age2check = Number(age)
+
+  if (age2check <= 0 || age.length === 0) {
+    console.log('Désoler, mais vous devez renseigner obligatoirement votre age et il ne peut pas être égale ou inférieur à 0.')
+    continue
+  }
 
   console.log(check(age, majo))
 }
