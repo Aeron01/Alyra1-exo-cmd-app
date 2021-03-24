6f661187fcd7a91aae6698ca6389841cfb8bd827
Aeron01 <one.soulreaper@gmail.com>
2021-03-24 12:28:16 +0100

    up

--- pyramid.js
+++ pyramid.js
@@ -1,3 +1,26 @@
+const { rgb } = require("chalk")
+
+if (process.argv.length !== 4) {
+  if (process.argv[4] === '-r') {
+    let rev = '-r'
+  } else if (process.argv.length !== 4) {
+    console.log('Usage: node pyramid.js number caractère -r')
+    process.exit(1)
+  } else {
+    console.log('Usage: node pyramid.js number caractère')
+    process.exit(1)
+  }
+}
+
+if (isNaN(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} is not a number.`)
+  process.exit(1)
+}
+
+let nb = Number(process.argv[2])
+let car = process.argv[3]
+let rev = process.argv[4]
+
 const showStars = (nbStars, caract, revert) => {
   if (rev === "-r") {
     for (let i = nbStars; i >= 1; i -= 1) {
@@ -11,23 +34,4 @@ const showStars = (nbStars, caract, revert) => {
   }
 }
 
-if (process.argv.length !== 4) {
-  if (process.argv[4] === '-r') {
-    let rev = '-r'
-  } else if (process.argv.length !== 4) {
-    console.log('Usage: node pyramid.js number caractère -r')
-    process.exit(1)
-  } else {
-    console.log('Usage: node pyramid.js number caractère')
-    process.exit(1)
-  }
-}
-if (isNaN(process.argv[2])) {
-  console.log(`Error: ${process.argv[2]} is not a number.`)
-  process.exit(1)
-}
-
-let nb = Number(process.argv[2])
-let car = process.argv[3]
-let rev = process.argv[4]
 showStars(nb, car, rev)
